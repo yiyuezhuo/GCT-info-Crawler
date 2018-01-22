479c7a168608ec1afb69b4251e51bba908827314
yiyuezhuo <616271835@163.com>
2018-01-22 22:34:39 +0800

    fix update error

---
+++ analysis.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 28 23:17:40 2017
+
+@author: my
+"""
+
+import os
+
+
+os.listdir('')
--- test.py
+++ test.py
@@ -10,8 +10,10 @@ from bs4 import BeautifulSoup
 import re
 import webbrowser
 import os
-import pickle
-#import json
+#import pickle
+import json
+#import shutil
+
 
 def fff(res,cache_name='tmp.html'):
     with open(cache_name,'wb') as f:
@@ -36,9 +38,23 @@ res3 = requests.get(url3)
 
 dmmc_list = res.json() # dm:序号，mc：学科类别 如0101 哲学 0811 控制科学与工程
 
-def dmmc_pageno_info(dmmc,pageno):
-    assert isinstance(pageno,str)
-    info = {'url_list':[],'name_list':[],'total':None,'now':None}
+def get_now_total(soup):
+    # fuck the page refactor
+    now = soup.select_one('li.selected').select_one('a').text
+    #total = soup.select_one('.ch-page').select('li')[-2].select_one('a').text
+    #text_list = [li.select_one('a').text for li in soup.select_one('.ch-page').select('li')]
+    #total = [text for text in text_list if text.isdigit()][-1]
+    text_list = []
+    for li in soup.select_one('.ch-page').select('li'):
+        if li.select_one('a'):
+            text = li.select_one('a').text
+            if text.isdigit():
+                text_list.append(text)
+    total = text_list[-1]
+    return now,total
+        
+    
+def dmmc_pageno_info_post(dmmc,pageno):
     url = 'http://yz.chsi.com.cn/zsml/queryAction.do'
     data = {'ssdm':'',
             'dwmc':'',
@@ -49,6 +65,24 @@ def dmmc_pageno_info(dmmc,pageno):
             'pageno':pageno, # '1'
     }
     res  = requests.post(url, data)
+    return res
+
+def dmmc_pageno_info(dmmc,pageno):
+    assert isinstance(pageno,str)
+    info = {'url_list':[],'name_list':[],'total':None,'now':None}
+    '''
+    url = 'http://yz.chsi.com.cn/zsml/queryAction.do'
+    data = {'ssdm':'',
+            'dwmc':'',
+            'mldm':dmmc['dm'][:2], # '08'
+            'mlmc':dmmc['mc'], # ‘工学’
+            'yjxkdm':dmmc['dm'], # '0811'
+            'zymc':'',
+            'pageno':pageno, # '1'
+    }
+    res  = requests.post(url, data)
+    '''
+    res = dmmc_pageno_info_post(dmmc,pageno)
     soup = BeautifulSoup(res.content,"lxml")
     table = soup.select('table')[-1]
     for tr in table.select('tr'):
@@ -58,21 +92,28 @@ def dmmc_pageno_info(dmmc,pageno):
         href = a[0].attrs['href']
         info['name_list'].append(a[0].text)
         info['url_list'].append(host + href.replace('amp;',''))
-    now,total = soup.select('#page_total')[0].text.split('/')
+    #now,total = soup.select('#page_total')[0].text.split('/')
+    now,total = get_now_total(soup)
     info['now'] = int(now)
     info['total'] = int(total)
     return info
     
 def major_examination_info(major_examination_url):
+    #print(major_examination_url)
     res = requests.get(major_examination_url)
     soup = BeautifulSoup(res.content,'lxml')
     method_l = []
-    for tr in soup.select('tr'):
-        tdl = tr.select('td')
-        if len(tdl) != 5:
-            continue
-        text_l = [td.text  for td in tdl]
-        method_l.append(text_l[1:])
+    for tbody in soup.select('.zsml-res-items'):
+        tbody_content = []
+        for td in tbody.select('td'):
+            '''
+            <td>
+                            (101)思想政治理论
+                            <span class="sub-msg">见招生简章</span>
+            </td>
+            '''
+            tbody_content.append(td.next.strip())
+        method_l.append(tbody_content)
     return method_l
             
     
@@ -88,6 +129,7 @@ def dmmc_to_school(dmmc):
         url_list.extend(info['url_list'])
         name_list.extend(info['name_list'])
     return {'name_list':name_list, 'url_list':url_list}
+
     
 def school_info(school_page_url):
     info = {'major_list':[],'total':None,'now':None}
@@ -108,7 +150,8 @@ def school_info(school_page_url):
         script_text = str(tr.select('script')[1].text)
         major['number'] = re.findall(r"cutString\('(.*)',6\)",script_text)[0]
         info['major_list'].append(major)
-    now,total = soup.select('#page_total')[0].text.split('/')
+    #now,total = soup.select('#page_total')[0].text.split('/')
+    now,total = get_now_total(soup)
     info['now'] = int(now)
     info['total'] = int(total)
     return info
@@ -125,7 +168,7 @@ def school_to_recruit_examation(school_url):
         major_list.extend(info['major_list'])
     return {'major_list':major_list}
 
-def dmmc_to_major(dmmc,disp=True,retry_limit=10):
+def dmmc_to_major(dmmc,disp=True,retry_limit=10,cache_dir='cache',instant=True):
     schools = dmmc_to_school(dmmc)
     major_list = []
     for school_name,school_url in zip(schools['name_list'],schools['url_list']):
@@ -134,37 +177,93 @@ def dmmc_to_major(dmmc,disp=True,retry_limit=10):
         
         for i in range(retry_limit):
             try:
-                major_list_part = school_to_recruit_examation(school_url)['major_list']
+                if not instant:
+                    
+                    major_list_part = school_to_recruit_examation(school_url)['major_list']
+                    
+                else:
+                    root = os.path.join(cache_dir,dmmc['dm'])
+                    if not os.path.isdir(root):
+                        os.makedirs(root)
+                    path = os.path.join(root,school_name)
+                    
+                    if os.path.isfile(path):
+                        print('pass {}'.format(school_name))
+                    else:
+                        
+                        major_list_part = school_to_recruit_examation(school_url)['major_list']
+                        for major in major_list_part:
+                            major['school'] = school_name
+
+                        with open(path, 'w') as f:
+                            #pickle.dump(result, f)
+                            json.dump(major_list_part, f , ensure_ascii=False, indent=1)
                 break
-            except ConnectionError:
+            except requests.exceptions.ConnectionError:
                 if disp:
                     print('Connecting fail try {}/{}'.format(i+1,retry_limit))
         else:
             raise ConnectionAbortedError
-        
-        for major in major_list_part:
-            major['school'] = school_name
-        major_list.extend(major_list_part)
+        if not instant:
+            for major in major_list_part:
+                major['school'] = school_name
+            major_list.extend(major_list_part)
     return major_list
     
-def download_dmmc_list(dmmc_list,disp=True,cache_dir='cache'):
+def download_dmmc_list(dmmc_list,disp=True,cache_dir='cache',instant=True):
     os.makedirs(cache_dir,exist_ok=True)
     for dmmc in dmmc_list:
-        path = os.path.join(cache_dir,dmmc['dm'])
+        if not instant:
+            path = os.path.join(cache_dir,dmmc['dm'])
         
-        if os.path.exists(path):
+        
+        if (not instant) and os.path.isfile(path):
             if disp:
                 print('Skip {} {}'.format(dmmc['dm'],dmmc['mc']))
             continue
         
         if disp:
             print('Start Collecting {} {}'.format(dmmc['dm'],dmmc['mc']))
-        result = dmmc_to_major(dmmc, disp = disp)
         
-        with open(path, 'wb') as f:
-            pickle.dump(result, f)
-
+        root = os.path.join(cache_dir,dmmc['dm'])
+        if instant:
+            if not os.path.isdir(root):
+                os.makedirs(root)
+                with open(os.path.join(root,'.uncompleted'),'w'):
+                    pass
+                
+        if (not instant) or os.path.isfile(os.path.join(root,'.uncompleted')):
+            result = dmmc_to_major(dmmc, disp = disp,cache_dir=cache_dir,instant=instant)
+        else:
+            print('skip {}'.format(dmmc['dm']))
+        
+        if instant and os.path.isfile(os.path.join(root,'.uncompleted')):
+            os.remove(os.path.join(root,'.uncompleted'))
+            with open('completed','w'):
+                pass
+        
+        if not instant:
+            with open(path, 'w') as f:
+                #pickle.dump(result, f)
+                json.dump(result, f, ensure_ascii=False, indent=1)
+        
     
-def doanload_all(disp=True,cache_dir='cache'):
-    dmmc_list = requests.get(url)
+def download_all(disp=True,cache_dir='cache'):
+    dmmc_list = requests.get(url).json()
     download_dmmc_list(dmmc_list,disp=disp,cache_dir=cache_dir)
+    
+def download_sci_tech(disp=True,cache_dir='cache'):
+    dmmc_list = requests.get(url).json()
+    dmmc_list = [dmmc for dmmc in dmmc_list if dmmc['dm'][:2] in ['07','08']]
+    download_dmmc_list(dmmc_list,disp=disp,cache_dir=cache_dir)
+
+DEBUG = False
+
+if DEBUG:
+    dmmc_list = requests.get(url).json()
+    dmmc = dmmc_list[0]
+    res = dmmc_pageno_info_post(dmmc,'1')
+    soup = BeautifulSoup(res.content,"lxml")
+    
+    now = soup.select_one('li.selected').select_one('a').text
+    total = soup.select_one('.ch-page').select('li')[-2].select_one('a').text
